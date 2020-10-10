fa0d4f9f3f4b445310153b07cc151ecb5085c171
admantiumblack <felixroland12355@gmail.com>
2020-10-10 18:48:34 +0700

    changed to backend

--- src/config/constants.js
+++
@@ -1,5 +0,0 @@
-export const TOKEN = 'CAFLANLELXWMEYRBNWJA';
-export const PATH_BASE = 'https://www.eventbriteapi.com/v3';
-export const PATH_CATEGORIES = '/categories';
-export const PATH_EVENTS = '/events/search';
-export const PARAM_LOCALE = 'locale=es_ES';
